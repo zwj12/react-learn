45113cc678ea590ec133420dadcf4ecd1abfa19c
zwjtest1 <zwj_12@163.com>
2019-04-20 22:38:05 +0800

    df

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React by 
+            Learn React by sdfsdf
           </a>
         </header>
       </div>
