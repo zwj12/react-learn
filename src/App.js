5437764fd9bb3d4871e2670e67aeecc8b7bc4995
zwjtest1 <zwj_12@163.com>
2019-04-20 22:36:12 +0800

    e

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React by Mich
+            Learn React by 
           </a>
         </header>
       </div>
